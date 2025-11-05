b232eecf70f99f6b6418307ede39497ac13e01de
Luiz Campos <campos.antoonio@gmail.com>
2025-11-05 12:46:13 -0300

    feat(gateway): enable websocket routing through reactive gateway

--- reactive-gateway/src/main/java/com/mochaeng/theia/reactive_gateway/infrastructure/config/SecurityConfig.java
+++ reactive-gateway/src/main/java/com/mochaeng/theia/reactive_gateway/infrastructure/config/SecurityConfig.java
@@ -1,25 +1,67 @@
 package com.mochaeng.theia.reactive_gateway.infrastructure.config;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
+
+@Slf4j
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http) {
         return http
-            .authorizeExchange(exchanges -> exchanges
-                .pathMatchers(HttpMethod.GET, "/ws/**").permitAll()
-                .anyExchange().authenticated()
+            .authorizeExchange(exchanges ->
+                exchanges
+                    .pathMatchers("/ws/**")
+                    .permitAll()
+                    .anyExchange()
+                    .authenticated()
             )
             .oauth2ResourceServer(oauth2ResourceServer ->
-                oauth2ResourceServer.jwt(Customizer.withDefaults())
+                oauth2ResourceServer.jwt(
+                    Customizer.withDefaults()
+                )
+            )
+            .exceptionHandling(ex -> ex
+                .accessDeniedHandler((exchange, denied) -> {
+                    log.info("access denied for path '{}'", exchange.getRequest().getPath());
+                    return Mono.error(denied);
+                })
             )
             .build();
     }
 
+
+//    @Bean
+//    public Converter<Jwt, Mono<AbstractAuthenticationToken>> reactiveJwtAuthenticationConverter() {
+//        var converter = new JwtAuthenticationConverter();
+//
+//        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+//            if (realmAccess == null || !realmAccess.containsKey("roles")) {
+//                return List.of();
+//            }
+//
+//            var rolesObj = realmAccess.get("roles");
+//            if (!(rolesObj instanceof List<?> roles)) {
+//                return List.of();
+//            }
+//
+//            return roles
+//                .stream()
+//                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+//                .collect(Collectors.toList());
+//        });
+//
+//        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+//    }
 }
